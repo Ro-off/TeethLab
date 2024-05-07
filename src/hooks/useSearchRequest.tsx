3c6b48ef162172d55a1369ed6b12e4805db3c2f0
Your Name <romansk2001@gmail.com>
2024-05-07 19:29:22 +0300

    feat: Add useSearchRequest hook for managing search request state

---
+++ src/hooks/useSearchRequest.tsx
@@ -0,0 +1,28 @@
+import { DateValue } from "@nextui-org/react";
+import { createGlobalState } from "react-hooks-global-state";
+
+const initialState = {
+  searchRequest: {
+    searchString: null,
+    orderer: null,
+    patient: null,
+    technician: null,
+    dateRange: null,
+  },
+};
+
+const { useGlobalState } = createGlobalState(initialState);
+
+export function useSearchRequest() {
+  const [searchRequest, setSearchRequest] = useGlobalState("searchRequest");
+
+  function setSearchRequestField(
+    field: string,
+    value: string | null | { start: DateValue | null; end: DateValue | null }
+  ) {
+    setSearchRequest((prev) => ({ ...prev, [field]: value }));
+  }
+  //   console.log(searchRequest);
+
+  return { searchRequest, setSearchRequestField };
+}
