0918de43115ae6ed2b5c31f13afaabea81b3a683
Your Name <romansk2001@gmail.com>
2024-05-10 01:51:59 +0300

    refactor: Update useTableData hook to use server-side data fetching and search functionality

--- src/hooks/useTableData.tsx
+++ src/hooks/useTableData.tsx
@@ -1,30 +1,36 @@
-import { data } from "../tableData.json";
-import { useState, useEffect } from "react";
+// import { data } from "../tableData.json";
+// import { useState, useEffect } from "react";
+import { useServerTableData } from "./useServerTableData";
+import { useSearchRequest } from "./useSearchRequest";
 
-function getData(offset: number, numberOfStrings: number) {
-  const tableData = data;
-  const tableDataLength = tableData.length;
-  const tableDataSlice = tableData.slice(
-    tableDataLength - offset - numberOfStrings,
-    tableDataLength - offset
-  );
-  return tableDataSlice;
+function createSearchQuery(
+  searchRequest: searchRequest,
+  offset: number,
+  limit: number
+) {
+  const searchQuery = new URLSearchParams();
+  for (const [key, value] of Object.entries(searchRequest)) {
+    if (value) {
+      searchQuery.append(key, value.toString());
+    }
+  }
+  searchQuery.append("offset", offset.toString());
+  searchQuery.append("limit", limit.toString());
+  return searchQuery;
 }
 
 export const useTableData = (offset: number, numberOfStrings: number) => {
-  const [data, setData] = useState<
-    {
-      id: number;
-      client: string;
-      patient: string;
-      date: string;
-      technician: string;
-      comments: string;
-    }[]
-  >([]);
+  const { searchRequest } = useSearchRequest();
+  const paramsQuery = createSearchQuery(searchRequest, offset, numberOfStrings);
+  const { data, total, isLoading } = useServerTableData(paramsQuery);
 
-  useEffect(() => {
-    setData(() => [...data, ...getData(offset, numberOfStrings)]);
-  }, [offset, numberOfStrings]);
-  return data;
+  return { data, total, isLoading };
+};
+
+type searchRequest = {
+  searchString: string | null;
+  client: string | null;
+  patient: string | null;
+  technician: string | null;
+  dateRange: { start: Date | null; end: Date | null } | null;
 };
