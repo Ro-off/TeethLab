0f7cae745d18081d80a365935f6f5d3a8b3624ab
Your Name <romansk2001@gmail.com>
2024-05-10 17:11:47 +0300

    refactor: Update JobsTable component to use useAsyncList and useInfiniteScroll for fetching and displaying table data

--- src/components/JobsTable/JobsTable.tsx
+++ src/components/JobsTable/JobsTable.tsx
@@ -6,55 +6,146 @@ import {
   TableRow,
   TableCell,
   getKeyValue,
-  Button,
+  Spinner,
 } from "@nextui-org/react";
-import { useTableData } from "../../hooks/useTableData";
+// import { useTableData } from "../../hooks/useTableData";
+import { useAsyncList } from "@react-stately/data";
+import { useInfiniteScroll } from "@nextui-org/use-infinite-scroll";
+import { useState } from "react";
 
 export function JobsTable({
   numberOfStrings,
   offset,
   setOffset,
 }: JobsTableProps) {
-  const { data, isLoading } = useTableData(offset, numberOfStrings);
+  console.log("JobsTable");
+  console.log(
+    `numberOfStrings: ${numberOfStrings}, offset: ${offset}, setOffset${setOffset}`
+  );
+  // const { data, isLoading: isLoading2 } = useTableData(offset, numberOfStrings);
 
-  const columns = [
-    {
-      key: "client",
-      label: "CLIENT",
-    },
-    {
-      key: "patient",
-      label: "PATIENT",
-    },
-    {
-      key: "technician",
-      label: "TECHNICIAN",
-    },
-  ];
+  // const columns = [
+  //   {
+  //     key: "client",
+  //     label: "CLIENT",
+  //   },
+  //   {
+  //     key: "patient",
+  //     label: "PATIENT",
+  //   },
+  //   {
+  //     key: "technician",
+  //     label: "TECHNICIAN",
+  //   },
+  // ];
 
-  function loadMoreTableData() {
-    setOffset(offset + numberOfStrings);
-  }
-  return isLoading ? (
-    <p>Loading...</p>
-  ) : (
+  // function loadMoreTableData() {
+  //   setOffset(offset + numberOfStrings);
+  // }
+  // return isLoading ? (
+  //   <p>Loading...</p>
+  // ) : (
+  //   <Table
+  //     color="primary"
+  //     selectionMode="multiple"
+  //     defaultSelectedKeys={["2", "3"]}
+  //     aria-label="Example static collection table"
+  //     // isHeaderSticky
+  //     classNames={{ base: "max-h-full" }}
+  //     bottomContent={<Button onPress={loadMoreTableData}>Load more</Button>}
+  //   >
+  //     <TableHeader columns={columns}>
+  //       {(column: column) => (
+  //         <TableColumn key={column.key}>{column.label}</TableColumn>
+  //       )}
+  //     </TableHeader>
+  //     <TableBody items={data || []}>
+  //       {(item) => (
+  //         <TableRow key={item.id}>
+  //           {(columnKey) => (
+  //             <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+  //           )}
+  //         </TableRow>
+  //       )}
+  //     </TableBody>
+  //   </Table>
+  // );
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(false);
+
+  const list = useAsyncList({
+    async load({ signal, cursor }) {
+      if (cursor) {
+        setIsLoading(false);
+      }
+
+      // If no cursor is available, then we're loading the first page.
+      // Otherwise, the cursor is the next URL to load, as returned from the previous page.
+      const res = await fetch(
+        cursor || "https://swapi.py4e.com/api/people/?search=",
+        { signal }
+      );
+      const json = await res.json();
+
+      setHasMore(json.next !== null);
+
+      return {
+        items: json.results,
+        cursor: json.next,
+      };
+    },
+  });
+
+  const [loaderRef, scrollerRef] = useInfiniteScroll({
+    hasMore,
+    onLoadMore: list.loadMore,
+  });
+
+  return (
     <Table
-      color="primary"
-      selectionMode="multiple"
-      defaultSelectedKeys={["2", "3"]}
-      aria-label="Example static collection table"
-      // isHeaderSticky
-      classNames={{ base: "max-h-full" }}
-      bottomContent={<Button onPress={loadMoreTableData}>Load more</Button>}
+      isHeaderSticky
+      aria-label="Example table with infinite pagination"
+      baseRef={scrollerRef}
+      bottomContent={
+        hasMore ? (
+          <div className="flex w-full justify-center">
+            <Spinner ref={loaderRef} color="white" />
+          </div>
+        ) : null
+      }
+      classNames={{
+        base: "max-h-[calc(100vh-4rem)] overflow-scroll",
+        table: "min-h-[400px]",
+      }}
     >
-      <TableHeader columns={columns}>
-        {(column: column) => (
-          <TableColumn key={column.key}>{column.label}</TableColumn>
-        )}
+      <TableHeader>
+        <TableColumn key="name">Name</TableColumn>
+        <TableColumn key="height">Height</TableColumn>
+        <TableColumn key="mass">Mass</TableColumn>
+        <TableColumn key="birth_year">Birth year</TableColumn>
       </TableHeader>
-      <TableBody items={data || []}>
-        {(item) => (
-          <TableRow key={item.id}>
+      <TableBody
+        isLoading={isLoading}
+        items={
+          list.items as {
+            name: string;
+            height: string;
+            mass: string;
+            hair_color: string;
+            skin_color: string;
+          }[]
+        }
+        loadingContent={<Spinner color="white" />}
+      >
+        {(item: {
+          name: string;
+          height: string;
+          mass: string;
+          hair_color: string;
+          skin_color: string;
+        }) => (
+          <TableRow key={item.name}>
             {(columnKey) => (
               <TableCell>{getKeyValue(item, columnKey)}</TableCell>
             )}
@@ -70,7 +161,7 @@ type JobsTableProps = {
   setOffset: (offset: number) => void;
 };
 
-type column = {
-  key: string;
-  label: string;
-};
+// type column = {
+//   key: string;
+//   label: string;
+// };
