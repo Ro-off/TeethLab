60be407ac7f30553ec3ee382ad80420b6e5fda5f
Your Name <romansk2001@gmail.com>
2024-05-11 16:12:32 +0300

    refactor: Clear code in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-// import './App.css'
-// import * as React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import { Header } from "./components/Header/Header";
 import { JobsTable } from "./components/JobsTable/JobsTable";
-import { useState } from "react";
 
 function App() {
-  const [numberOfStrings] = useState(5);
-  const [offset, setOffset] = useState(0);
   return (
     <>
       <NextUIProvider>
         <Header />
-        <JobsTable
-          numberOfStrings={numberOfStrings}
-          offset={offset}
-          setOffset={setOffset}
-        />
+        <JobsTable />
       </NextUIProvider>
     </>
   );
